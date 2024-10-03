4303e017c552823b92a52e6fdf897de32461931b
bparungo <bparungo@web.de>
2024-10-03 19:58:50 +0200

    add pwa support

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-import ThemeToggle from "./components/theme-toggle.vue";
+import { ref } from "vue";
 import main_view from "./views/main-view.vue";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+
+const deferredPrompt = ref();
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log(e);
+  deferredPrompt.value = e;
+});
 </script>
 
 <template>
   <div class="main-container">
     <main_view />
   </div>
+  <Card
+    class="absolute bottom-0 rounded-none w-full h-32 flex items-center justify-center"
+    v-if="deferredPrompt"
+  >
+    <Button @click="deferredPrompt.prompt()"> App installieren</Button>
+  </Card>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
-
+import { VitePWA } from "vite-plugin-pwa";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
@@ -11,7 +11,34 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["shoppingcart.svg"],
+      manifest: {
+        name: "Shoppinglist Helper",
+        short_name: "Shoppinglist Helper",
+        description: "A simple shopping list helper",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "shoppingcart.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "shoppingcart.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
